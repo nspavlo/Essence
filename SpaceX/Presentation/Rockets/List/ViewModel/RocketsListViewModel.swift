ab100ab5214230bde88395675e1952b76d2be742
Jans Pavlovs <jans.pavlovs@gmail.com>
2021-01-30 11:30:39 +0200

    Extract remote repository

--- SpaceX/Presentation/Rockets/List/ViewModel/RocketsListViewModel.swift
+++ SpaceX/Presentation/Rockets/List/ViewModel/RocketsListViewModel.swift
@@ -70,24 +70,3 @@ extension RocketsListController {
         showRocketDetails?(rockets[indexPath.row])
     }
 }
-
-// MARK: -
-
-// MARK: Initialization
-
-final class RemoteRocketsRepository {
-    let dispatcher: RequestDispatcher
-
-    init(dispatcher: RequestDispatcher) {
-        self.dispatcher = dispatcher
-    }
-}
-
-// MARK: RocketsRepository
-
-extension RemoteRocketsRepository: RocketsRepository {
-    func fetch(with completion: @escaping (Result<Rockets, RequestError>) -> Void) -> Cancellable? {
-        let request = Request(method: .get, endpoint: .rockets, params: nil)
-        return dispatcher.execute(request, completion: completion)
-    }
-}
