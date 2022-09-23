a2264049d5b756663cf64d76c8c95ffda3567393
Jans Pavlovs <jans.pavlovs@gmail.com>
2022-09-23 12:17:35 +0300

    Remove redundant init override from `RocketsTableViewCell`

--- Essence/Presentation/Rockets/List/View/RocketsTableViewCell.swift
+++ Essence/Presentation/Rockets/List/View/RocketsTableViewCell.swift
@@ -9,16 +9,7 @@ import UIKit
 
 // MARK: Initialization
 
-final class RocketsTableViewCell: UITableViewCell {
-    override init(style _: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
-    }
-
-    @available(*, unavailable)
-    required init?(coder _: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
+final class RocketsTableViewCell: UITableViewCell {}
 
 // MARK: Configuration
 
